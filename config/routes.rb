dcfe3d8fc23d8fb47c80e4c9e06ab62236725d3f
Sean OReilly <soreilly424@gmail.com>
2020-12-18 16:41:50 -0500

    changed paths to get rid of the home page for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#sealife'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/sealife", to: 'pages#sealife'
 end
